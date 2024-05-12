bb0f5d061688300438c753cfdd95da32ed943bd5
RezkiBawono <rezkibawono@gmail.com>
2024-05-12 16:38:56 +0700

    adding updateQuantity to CartContext

--- src/app/provider/CartContext.tsx
+++ src/app/provider/CartContext.tsx
@@ -5,11 +5,13 @@ import { randomUUID } from "expo-crypto";
 type CartProvider = {
   items: CartItem[];
   addItem: (product: Product, size: CartItem["size"]) => void;
+  updateQuantity: (itemId: string, amount: -1 | 1) => void;
 };
 
 export const CartContext = createContext<CartProvider>({
   items: [],
   addItem: () => {},
+  updateQuantity: () => {},
 });
 //This variable is a default and adress to TS standart
 
@@ -30,10 +32,20 @@ export default function CartContextProvider({ children }: PropsWithChildren) {
     setItems([newItemCart, ...items]);
   };
 
+  const updateQuantity = (itemId: string, amount: -1 | 1) => {
+    const updatedItems = items
+      .map((item) =>
+        item.id == itemId ? { ...item, quantity: item.quantity + amount } : item
+      )
+      .filter((item) => item.quantity > 0);
+    setItems(updatedItems);
+  };
+  //This function is used to update an item in the cart
+
   // TODO : update quantity
 
   return (
-    <CartContext.Provider value={{ items, addItem }}>
+    <CartContext.Provider value={{ items, addItem, updateQuantity }}>
       {children}
     </CartContext.Provider>
   );

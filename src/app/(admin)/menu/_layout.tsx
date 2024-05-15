f44308ff371cce3a5b0a34456b6058ef440e0daa
RezkiBawono <rezkibawono@gmail.com>
2024-05-15 14:59:24 +0700

    fix issues

--- src/app/(admin)/menu/_layout.tsx
+++ src/app/(admin)/menu/_layout.tsx
@@ -8,16 +8,39 @@ export default function MenuStack() {
   return (
     <Stack screenOptions={{}}>
       <Stack.Screen
-        name="[id]"
+        name="index"
         options={{
           title: "Menu",
           headerTitleAlign: "center",
           headerRight: () => (
-            <Link href="/cart" asChild>
+            <Link href="/" asChild>
+              {/* TODO adding create a pizza screen */}
               <Pressable>
                 {({ pressed }) => (
                   <FontAwesome
                     name="plus"
+                    size={25}
+                    color={Colors.light.tint}
+                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                  />
+                )}
+              </Pressable>
+            </Link>
+          ),
+        }}
+      />
+
+      <Stack.Screen
+        name="[id]"
+        options={{
+          headerTitleAlign: "center",
+          headerRight: () => (
+            <Link href="/" asChild>
+              {/* TODO adding edit pizza screen */}
+              <Pressable>
+                {({ pressed }) => (
+                  <FontAwesome
+                    name="pencil"
                     size={25}
                     color={Colors.light.tint}
                     style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}

2e987addb289229dda067593c4f039cc4ccde8ec
RezkiBawono <rezkibawono@gmail.com>
2024-05-21 02:07:44 +0700

    implementing a feature to not obstruct forms with keyboard

--- src/app/(admin)/menu/CreatePizzaScreen.tsx
+++ src/app/(admin)/menu/CreatePizzaScreen.tsx
@@ -1,11 +1,25 @@
-import { StyleSheet, Text, View, Image, TextInput } from "react-native";
-import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  KeyboardAvoidingView,
+  Keyboard,
+  Platform,
+  ScrollView,
+} from "react-native";
+import React, { useEffect, useState } from "react";
 import { useForm, Controller } from "react-hook-form";
 import Button from "@/components/Button";
 import defaultImageLink from "@/constants/DefaultImage";
 import * as ImagePicker from "expo-image-picker";
 import Colors from "@/constants/Colors";
-import { Stack } from "expo-router";
+
+// TODO : create error handling
+// TODO : make sure the keyboard doesnt obstruct the form
+// TODO : make a feature to go back after you submit the
+// TODO : create a form validation
 
 type FormData = {
   name: string;
@@ -18,8 +32,25 @@ const CreatePizzaScreen = () => {
     handleSubmit,
     formState: { errors },
   } = useForm<FormData>();
+  const [keyboardVisible, setKeyboardVisible] = useState(false);
   const [image, setImage] = useState<string | null>(null);
   const [submittedData, setSubmittedData] = useState<FormData>();
+
+  useEffect(() => {
+    const keyboardDidShowListener = Keyboard.addListener(
+      "keyboardDidShow",
+      () => setKeyboardVisible(true)
+    );
+    const keyboardDidHideListener = Keyboard.addListener(
+      "keyboardDidHide",
+      () => setKeyboardVisible(false)
+    );
+
+    return () => {
+      keyboardDidShowListener.remove();
+      keyboardDidHideListener.remove();
+    };
+  }, []);
 
   const onSubmit = (data: FormData) => {
     console.log("submittedData :", data);
@@ -43,68 +74,79 @@ const CreatePizzaScreen = () => {
   };
 
   return (
-    <View style={styles.container}>
-      <Image
-        style={styles.image}
-        source={{ uri: image || defaultImageLink }}
-        resizeMode="contain"
-      />
-      <Text onPress={pickImage} style={styles.textButton}>
-        Select Image
-      </Text>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "android" ? "padding" : "height"}
+      style={styles.container}
+    >
+      <ScrollView style={styles.scrollView}>
+        <Image
+          style={styles.image}
+          source={{ uri: image || defaultImageLink }}
+          resizeMode="contain"
+        />
+        <Text onPress={pickImage} style={styles.textButton}>
+          Select Image
+        </Text>
 
-      <Text style={styles.text}>Name</Text>
-      <Controller
-        control={control}
-        render={({ field: { onChange, onBlur, value } }) => (
-          <TextInput
-            onBlur={onBlur}
-            onChangeText={onChange}
-            value={value}
-            style={styles.input}
-            placeholder="Margarita.."
-          />
-        )}
-        name="name"
-      />
+        <Text style={styles.text}>Name</Text>
+        <Controller
+          control={control}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+              style={[
+                styles.input,
+                keyboardVisible && styles.inputWithKeyboard,
+              ]}
+              placeholder="Margarita.."
+            />
+          )}
+          name="name"
+        />
 
-      <Text style={styles.text}>Price</Text>
-      <Controller
-        control={control}
-        render={({ field: { onBlur, onChange, value } }) => (
-          <TextInput
-            onBlur={onBlur}
-            onChangeText={onChange}
-            value={value}
-            style={styles.input}
-            placeholder="$9.99"
-          />
+        <Text style={styles.text}>Price</Text>
+        <Controller
+          control={control}
+          render={({ field: { onBlur, onChange, value } }) => (
+            <TextInput
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+              style={[
+                styles.input,
+                keyboardVisible && styles.inputWithKeyboard,
+              ]}
+              placeholder="$9.99"
+            />
+          )}
+          name="price"
+        />
+        <Button text="Create a Pizza" onPress={handleSubmit(onSubmit)} />
+
+        {/* {errors.name && <Text style={styles.errorText}>{errors.name}</Text>} */}
+        {submittedData && (
+          <View style={styles.submittedContainer}>
+            <Text style={styles.submittedTitle}>Submitted Data:</Text>
+            <Text>Name: {submittedData.name}</Text>
+            <Text>Price: {submittedData.price}</Text>
+          </View>
         )}
-        name="price"
-      />
-      <Button text="Create a Pizza" onPress={handleSubmit(onSubmit)} />
-      {/* {errors.name && <Text style={styles.errorText}>{errors.name}</Text>} */}
-      {submittedData && (
-        <View style={styles.submittedContainer}>
-          <Text style={styles.submittedTitle}>Submitted Data:</Text>
-          <Text>Name: {submittedData.name}</Text>
-          <Text>Price: {submittedData.price}</Text>
-        </View>
-      )}
-    </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
-
-// TODO : create error handling
-// TODO : make sure the keyboard doesnt obstruct the form
-// TODO : make a feature to go back after you submit the form
 
 export default CreatePizzaScreen;
 
 const styles = StyleSheet.create({
   container: {
-    padding: 14,
+    flex: 1,
+    padding: 10,
   },
+  keyboard: {},
+  scrollView: { marginHorizontal: 20 },
   image: {
     width: "70%",
     aspectRatio: 1,
@@ -125,6 +167,9 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     marginBottom: 10,
     padding: 8,
+  },
+  inputWithKeyboard: {
+    paddingBottom: 10,
   },
   errorText: {},
   submittedContainer: {

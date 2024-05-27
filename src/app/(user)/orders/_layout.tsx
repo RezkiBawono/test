67c52b754c6f709e78392bc80f697dfbaf82e5c6
RezkiBawono <rezkibawono@gmail.com>
2024-05-27 14:57:47 +0700

    fixing layout.ts

--- src/app/(user)/orders/_layout.tsx
+++ src/app/(user)/orders/_layout.tsx
@@ -3,7 +3,18 @@ import React from "react";
 import { Stack } from "expo-router/stack";
 
 const OrdersStack = () => {
-  return <Stack screenOptions={{ headerTitleAlign: "center" }}></Stack>;
+  return (
+    <Stack screenOptions={{ headerTitleAlign: "center" }}>
+      <Stack.Screen
+        name="index"
+        options={{
+          headerTitleAlign: "center",
+          headerTitle: "test",
+          headerShown: false,
+        }}
+      />
+    </Stack>
+  );
 };
 
 export default OrdersStack;

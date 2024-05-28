cb81742bff5b24fc1e7be05ca0051eebc6355784
RezkiBawono <rezkibawono@gmail.com>
2024-05-29 01:19:41 +0700

    adding OrderItemListItem.tsx and dynamic route for orders

--- src/app/(user)/orders/[id].tsx
+++ src/app/(user)/orders/[id].tsx
@@ -1,11 +1,28 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 import React from "react";
+import { useLocalSearchParams } from "expo-router";
+import orders from "../../../../assets/data/orders";
+import OrderItemListItem from "@/components/OrderItemListItem";
+import OrderListItems from "@/components/OrderListItems";
 
 const OrderDetailScreen = () => {
+  const { id } = useLocalSearchParams();
+
+  const order = orders.find((o) => o.id.toString() == id);
+
+  if (!order) {
+    return <Text>Order is not Found</Text>;
+  }
+
+  <OrderListItems order={order} />;
+
   return (
-    <View>
-      <Text>OrderDetailScreen</Text>
-    </View>
+    <FlatList
+      data={order.order_items}
+      renderItem={({ item }) => <OrderItemListItem item={item} />}
+      keyExtractor={(item) => item.id.toString()}
+      contentContainerStyle={{ gap: 10 }}
+    />
   );
 };
 

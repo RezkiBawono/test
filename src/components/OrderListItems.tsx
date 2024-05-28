386d3752a00aa9a232ffae79c1f0d66e720518bd
RezkiBawono <rezkibawono@gmail.com>
2024-05-28 13:34:21 +0700

    fixing index redirect adding OrderListItem.tsx

---
+++ src/components/OrderListItems.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+
+import React from "react";
+
+const OrderListItems = () => {
+  return (
+    <View>
+      <Text>OrderListItems</Text>
+    </View>
+  );
+};
+
+export default OrderListItems;
+
+const styles = StyleSheet.create({});

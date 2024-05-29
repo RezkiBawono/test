fea1b053a7eef3212d86b91b2814ca40deb3eb38
RezkiBawono <rezkibawono@gmail.com>
2024-05-29 14:36:00 +0700

    add more TODO

--- TODO.js
+++ TODO.js
@@ -3,4 +3,5 @@
 // TODO : make an update and delete Pizza product -DONE
 // TODO : create auth screen with sign in and create acc - DONE
 // TODO : make a password form hidden
-// TODO : fixing stack title in user order screen
+// TODO : fixing stack title in user order screen - DONE
+// TODO : making admin order screen

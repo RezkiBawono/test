b802e70aab2b137a387cb196a13441bf36d0c1d6
RezkiBawono <rezkibawono@gmail.com>
2024-05-30 04:20:03 +0700

    removing safe area view from admin order screen

--- src/app/(admin)/orders/archive_orders/_layout.tsx
+++ src/app/(admin)/orders/archive_orders/_layout.tsx
@@ -1,6 +1,5 @@
 import { withLayoutContext } from "expo-router";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
-import { SafeAreaView } from "react-native-safe-area-context";
 
 export const TopTabs = withLayoutContext(
   createMaterialTopTabNavigator().Navigator
@@ -8,10 +7,8 @@ export const TopTabs = withLayoutContext(
 
 export default function OrdersTabs() {
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: "white" }} edges={["top"]}>
-      <TopTabs>
-        <TopTabs.Screen name="index" options={{ title: "Active" }} />
-      </TopTabs>
-    </SafeAreaView>
+    <TopTabs>
+      <TopTabs.Screen name="index" options={{ title: "Active" }} />
+    </TopTabs>
   );
 }

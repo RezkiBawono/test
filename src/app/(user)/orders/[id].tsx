28669ff253cd9a801c6780731c42c63b0fbcafb4
RezkiBawono <rezkibawono@gmail.com>
2024-05-30 02:14:21 +0700

    fixing user order detail screen

--- src/app/(user)/orders/[id].tsx
+++ src/app/(user)/orders/[id].tsx
@@ -13,11 +13,13 @@ const OrderDetailScreen = () => {
   if (!order) {
     return <Text>Order is not Found</Text>;
   }
-  <OrderListItems order={order} />;
 
   return (
     <View>
       <Stack.Screen options={{ title: `Order # ${order.id}` }} />
+
+      <OrderListItems order={order} />
+
       <FlatList
         data={order.order_items}
         renderItem={({ item }) => <OrderItemListItem item={item} />}

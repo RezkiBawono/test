5fcafc77093ec3016240fc9d3db4bbdfe9454807
RezkiBawono <rezkibawono@gmail.com>
2024-06-01 01:45:32 +0700

    implementing fetch session and adding AuthSession

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,16 @@
 import { View, Text } from "react-native";
 import React from "react";
 import Button from "../components/Button";
-import { Link } from "expo-router";
+import { Link, Redirect } from "expo-router";
+import { useAuth } from "@/providers/AuthProvider";
 
 const IndexScreen = () => {
+  const { session } = useAuth();
+
+  if (!session) {
+    return <Redirect href={"/sign-in"} />;
+  }
+
   return (
     <View style={{ flex: 1, justifyContent: "center", padding: 10 }}>
       <Link href={"/(admin)"} asChild>
--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -1,13 +1,35 @@
-import { createContext, PropsWithChildren, useEffect } from "react";
+import { supabase } from "@/lib/supabase";
+import { Session } from "@supabase/supabase-js";
+import {
+  createContext,
+  PropsWithChildren,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 
-type AuthData = {};
+type AuthData = {
+  session: Session | null;
+};
 
-export const AuthContext = createContext<AuthData>({});
+export const AuthContext = createContext<AuthData>({
+  session: null,
+});
 
 export default function AuthContextProvider({ children }: PropsWithChildren) {
+  const [session, setSession] = useState<Session | null>(null);
+
   useEffect(() => {
-    console.log("AuthProvider is mounted");
+    const fetchSession = async () => {
+      const { data } = await supabase.auth.getSession();
+      setSession(data.session);
+    };
+    fetchSession();
   }, []);
 
-  return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider value={{ session }}>{children}</AuthContext.Provider>
+  );
 }
+
+export const useAuth = () => useContext(AuthContext);

ad4855915403958391bf83a541f64fb39bed6355
RezkiBawono <rezkibawono@gmail.com>
2024-06-01 02:26:40 +0700

    adding protected session navigation on AuthStack

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -1,8 +1,16 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { Stack } from "expo-router/stack";
+import { useAuth } from "@/providers/AuthProvider";
+import { Redirect } from "expo-router";
 
 const AuthStack = () => {
+  const { session } = useAuth();
+
+  if (session) {
+    return <Redirect href={"/"} />;
+  }
+
   return (
     <Stack screenOptions={{ headerTitleAlign: "center" }}>
       <Stack.Screen name="sign-in" options={{ title: "Sign In" }} />

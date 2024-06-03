568e4c38a7ba5e5c1263e7db3be15db1d57c480a
RezkiBawono <rezkibawono@gmail.com>
2024-06-03 17:22:10 +0700

    adding sign out button in profile user

--- src/app/(user)/profile.tsx
+++ src/app/(user)/profile.tsx
@@ -1,10 +1,13 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { supabase } from "@/lib/supabase";
+import Button from "@/components/Button";
 
 const Profile = () => {
   return (
     <View>
       <Text>profile</Text>
+      <Button text="Sign Out" onPress={() => supabase.auth.signOut()} />
     </View>
   );
 };

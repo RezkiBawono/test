2fb68b988537af66b5ed8ed6fa7735644ce8c88b
RezkiBawono <rezkibawono@gmail.com>
2024-06-04 14:26:31 +0700

    adding QueryProvider.tsx

---
+++ src/providers/QueryProvider.tsx
@@ -0,0 +1,14 @@
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
+import { PropsWithChildren } from "react";
+
+const client = new QueryClient();
+
+const QueryProvider = ({ children }: PropsWithChildren) => {
+  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+};
+
+export default QueryProvider;

0398ac4fc39f273bf820727b0dce42edf429509f
RezkiBawono <rezkibawono@gmail.com>
2024-06-04 14:17:59 +0700

    adding fetchProducts on user menu

--- src/app/(user)/menu/index.tsx
+++ src/app/(user)/menu/index.tsx
@@ -2,8 +2,19 @@ import { FlatList } from "react-native";
 
 import ProductListItems from "@/api/ProductListItems";
 import products from "../../../../assets/data/products";
+import { useEffect } from "react";
+import { supabase } from "@/lib/supabase";
 
 export default function TabOneScreen() {
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data, error } = await supabase.from("products").select("*");
+      console.log(data);
+      console.log(error);
+    };
+    fetchProducts();
+  }, []);
+
   return (
     <FlatList
       data={products}
